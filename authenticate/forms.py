085d07ff6773e2551071892ab66c30c8c20e0a84
SahebSoumik <soumik.manna10@gmail.com>
2021-11-18 15:28:36 +0530

    django project

---
+++ authenticate/forms.py
@@ -0,0 +1,42 @@
+from django.contrib.auth.forms import UserCreationForm,UserChangeForm
+from django.contrib.auth.models import User
+from django import forms
+
+class EditProfileForm(UserChangeForm):
+    password= forms.CharField(label="", widget=forms.TextInput(attrs={'type':'hidden'})) 
+
+    class Meta:
+        model = User
+        fields = ('username','first_name','last_name','email','password')
+
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(label="",widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Email Address'}))
+    first_name = forms.CharField(label="",max_length=100, widget=forms.TextInput(attrs={'class':'form-control','placeholder':'First name'})) 
+    last_name = forms.CharField(label="",max_length=100 ,widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Last name'}))
+
+    class Meta:
+        model = User
+        fields = ('username','first_name','last_name','email','password1','password2')
+
+    def __init__(self, *args, **kwargs):
+        super(SignUpForm,self).__init__(*args, **kwargs)
+
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['username'].label = ''
+        self.fields['username'].widget.attrs['placeholder'] = 'Enter username'
+        self.fields['username'].help_text = '<div class="form-text text-muted"><small>Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.</small></div>'
+
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].label = ''
+        self.fields['password1'].widget.attrs['placeholder'] = 'Enter password'
+        self.fields['password1'].help_text = '<div class="form-text text-muted"><small><ul><li>Your password can’t be too similar to your other personal information.</li><li>Your password must contain at least 8 characters.</li><li>Your password can’t be a commonly used password.</li><li>Your password can’t be a commonly used password.</li></small></div>'
+
+
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].label = ''
+        self.fields['password2'].widget.attrs['placeholder'] = 'Confirm password'
+        self.fields['password2'].help_text = '<div class="form-text text-muted"><small>Enter the same password</small></div>'
+
+
+
